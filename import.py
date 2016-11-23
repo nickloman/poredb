f954d8ec42e627d2a1bbd2c10dbd68e5ab68a515
Nicholas Loman <n.j.loman@bham.ac.uk>
2016-11-23 22:27:52 +0000

    poredb 0.000001

--- import.py
+++ import.py
@@ -1,5 +1,8 @@
 from poretools.poretools.Fast5File import Fast5File
 import sys
+import md5
+import hashlib
+import re
 
 """
 create table flowcell (
@@ -14,6 +17,7 @@ create table experiment (
   experiment_id varchar(100) not null,
   library_name varchar(40) not null,
   script_name varchar(100) not null,
+  host_name varchar(100) not null,
   exp_start_time integer not null,
   minion_id varchar(40) not null
 );
@@ -28,6 +32,8 @@ create table trackedfiles (
   sequenced_date integer not null
 );
 
+create index trackedfiles_uuid on trackedfiles ( uuid );
+
 -- basecaller
 create table basecaller (
   basecaller_id integer primary key not null,
@@ -36,14 +42,15 @@ create table basecaller (
 );
 
 -- basecealls
+-- TODO: complement
+-- TODO: 2d
+-- TODO: barcoding
+
 create table basecall (
   file_id integer references trackedfiles ( file_id ) not null,
-  basecaller integer references basecaller ( basecalled_id ) not null,
-  barcode varchar(10) null,
+  basecaller_id integer references basecaller ( basecaller_id ) not null,
   group_id integer not null,
-  template text not null,
-  complement text null,
-  twod text null
+  template text null,
 );
 """
 
@@ -61,21 +68,122 @@ def flowcell_get_or_create(flowcell_id, asic_id):
 	sql = "INSERT INTO flowcell ( flowcell_id, asic_id ) VALUES ( ?, ? )"
 	c.execute(sql, (flowcell_id, asic_id))
 
+def experiment_get_or_create(flowcell_id, experiment_id, library_name, script_name, exp_start_time, host_name, minion_id):
+	sql = "SELECT experiment_id FROM experiment WHERE experiment_id = ?"
+	c.execute(sql, (experiment_id,))
+	r = c.fetchone()
+	if r:
+		return
+
+	sql = "INSERT INTO experiment ( flowcell_id, experiment_id, library_name, script_name, exp_start_time, host_name, minion_id ) VALUES ( ?, ?, ?, ?, ?, ?, ? )"
+	c.execute(sql, (flowcell_id, experiment_id, library_name, script_name, exp_start_time, host_name, minion_id))
+
+def md5(fname):
+	hash_md5 = hashlib.md5()
+	with open(fname, "rb") as f:
+		for chunk in iter(lambda: f.read(1024*200), b""):
+			hash_md5.update(chunk)
+		return hash_md5.hexdigest()
+
+def trackedfiles_find(uuid):
+	sql = "SELECT file_id FROM trackedfiles WHERE uuid = ?"
+	c.execute(sql, (uuid,))
+	return c.fetchone()
+
+def trackedfiles_add(experiment_id, uuid, md5sig, filepath, sequenced_date):
+	sql = "INSERT INTO trackedfiles ( experiment_id, uuid, md5, filepath, sequenced_date ) VALUES ( ?, ?, ?, ?, ? )"
+	c.execute(sql, (experiment_id, uuid, md5sig, filepath, sequenced_date))
+	return c.lastrowid
+
+def get_basecaller_version(g):
+	try:
+		return g.attrs['chimaera version']
+	except:
+		pass
+
+	try:
+		return g.attrs['version']
+	except:
+		return None	
+
+def basecaller_get_or_delete(name, version):
+	sql = "SELECT basecaller_id FROM basecaller WHERE name = ? AND version = ?"
+	c.execute(sql, (name, version))
+	row = c.fetchone()
+	if row:
+		return int(row[0])
+
+	sql = "INSERT INTO basecaller ( name, version ) VALUES ( ?, ? )"
+	c.execute(sql, (name, version))
+	return c.lastrowid
+
+def basecall_add(read_id, basecaller_id, group, template):
+	sql = "INSERT INTO basecall ( file_id, basecaller_id, group_id, template ) VALUES ( ?, ?, ?, ? )"
+	c.execute(sql, (read_id, basecaller_id, group, template))
 
 def import_reads(fofn):
-	for fn in open(fofn):
-		fast5 = Fast5File(fn.rstrip())
-		flowcell_id = fast5.get_flowcell_id()
-		asic_id = fast5.get_asic_id()
-		flowcell_get_or_create(flowcell_id, asic_id)
-		print fn, fast5.get_flowcell_id()
+	matcher = re.compile('Basecall_1D_(\d+)')
 
-		experiment_id = fast5.get_experiment_id()
-		library_name = fast5.get_sample_name()
-		script_name = fast5.get_script_name()
-		exp_start_time = fast5.get_exp_start_time()
-		minion_id = fast5.get_device_id()
-		#experiment_get_or_create(flowcell_id, 
+	for fn in open(fofn):
+		fn = fn.rstrip()
+		fast5 = Fast5File(fn)
+
+		# how to handle files
+		# first - is uuid in database?
+		#   no -- add it as a tracked file - this is heuristic
+		#  yes -- is it the same file ?
+		#            check md5
+		#            if md5 different & path same -- update contents
+                #            if md5 same & path different -- update path
+		#            if md5 same & path same -- skip
+                #            or skip it
+
+		block = fast5.find_read_number_block_fixed_raw()
+		uuid = block.attrs['read_id']
+
+		if not trackedfiles_find(uuid):
+			# get flowcell
+			flowcell_id = fast5.get_flowcell_id()
+			asic_id = fast5.get_asic_id()
+			flowcell_get_or_create(flowcell_id, asic_id)
+
+			# get experiment
+			experiment_id = fast5.get_run_id()
+			library_name = fast5.get_sample_name()
+			script_name = fast5.get_script_name()
+			exp_start_time = fast5.get_exp_start_time()
+			host_name = fast5.get_host_name()
+			minion_id = fast5.get_device_id()
+
+			experiment_get_or_create(flowcell_id, experiment_id, library_name, script_name, exp_start_time, host_name, minion_id)
+
+			# add trackedfile
+			sequenced_date = int(block.attrs['start_time'])
+			sample_frequency = int(fast5.get_sample_frequency())
+			md5sig = md5(fn)
+			start_time = exp_start_time + (sequenced_date / sample_frequency)
+			read_id = trackedfiles_add(experiment_id, uuid, md5sig, fn, start_time)
+
+			# basecalls
+			analyses = fast5.hdf5file.get('Analyses')
+			for k, g in analyses.iteritems():
+				m = matcher.match(k)
+				if m:
+					basecaller_name = g.attrs['name']
+					group = m.group(1)
+					version = get_basecaller_version(g)
+
+					basecaller_id = basecaller_get_or_delete(basecaller_name, version)
+
+					try:
+						template = analyses.get("%s/BaseCalled_template" % (k,))['Fastq'][()]
+					except:
+						template = None
+
+					basecall_add(read_id, basecaller_id, group, template)
+				else:
+					print >>sys.stderr, "Skipping %s" % (k,)
+
 
 import_reads(sys.argv[2])
 conn.commit()
